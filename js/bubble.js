6f805237922f45771fb6fbb5d91e602cda0a68bc
debarun <debarun285@gmail.com>
2021-04-24 22:12:06 +0530

    second

--- js/bubble.js
+++
@@ -1,8 +0,0 @@
-var arr = [];
-
-while(arr.length < 100){
-    var r = Math.floor(Math.random() * 100) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
-}
-
-console.log(arr);
---
+++ js/sorting.js
@@ -0,0 +1,31 @@
+let array = [];
+
+// Call to display bars right when you visit the site
+createNewArray();
+
+// To create new array input size of array
+// use of default constructor
+function createNewArray(noOfBars = 60) {
+    // calling helper function to delete old bars from dom
+
+
+    // creating an array of random numbers 
+    array = [];
+    for (let i = 0; i < noOfBars; i++) {
+        array.push(Math.floor(Math.random() * 250) + 1);
+    }
+    console.log(array);
+
+    // select the div #bars element
+    const bars = document.querySelector("#bars");
+
+    // create multiple element div using loop and adding class 'bar col'
+    for (let i = 0; i < noOfBars; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = `${array[i]*2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bar.classList.add(`barNo${i}`);
+        bars.appendChild(bar);
+    }
+}
