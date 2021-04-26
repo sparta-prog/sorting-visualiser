03a473d8a9a37736aacfe4f2098bc2eaafc47f17
debarun <debarun285@gmail.com>
2021-04-26 23:10:00 +0530

    com

--- js/selection.js
+++ js/selection.js
@@ -1,10 +1,41 @@
-async function selection() {
-    console.log('Entered here');
-    const element = document.querySelectorAll(".bar");
+async function selection(){
+    console.log('In selection()');
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length; i++){
+        console.log('In ith loop');
+        let min_index = i;
+        // Change color of the position to swap with the next min
+        ele[i].style.background = '#7CFC00';
+        for(let j = i+1; j < ele.length; j++){
+            console.log('In jth loop');
+            // Change color for the current comparision (in consideration for min_index)
+            ele[j].style.background = 'red';
+
+            await animate(delay);
+            if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
+                console.log('In if condition height comparision');
+                if(min_index !== i){
+                    // new min_index is found so change prev min_index color back to normal
+                    ele[min_index].style.background = 'cyan';
+                }
+                min_index = j;
+            } 
+            else{
+                // if the currnent comparision is more than min_index change is back to normal
+                ele[j].style.background = 'cyan';
+            }   
+        }
+        await animate(delay);
+        swap(ele[min_index], ele[i]);
+        // change the min element index back to normal as it is swapped 
+        ele[min_index].style.background = 'cyan';
+        // change the sorted elements color to green
+        ele[i].style.background = 'yellow';
+    }
 }
 
 const selectionSortBtn = document.querySelector(".selectionSort");
-selectionSortBtn.addEventListener('click', () => {
+selectionSortBtn.addEventListener('click', async function () {
     disableSortingBtn();
     disableNewArrayBtn();
     disableSizeSlider();
